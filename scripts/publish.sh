b3caeba8b3d65c4fb24ef4a5afc543a4ff5610fa
trafalgarlwa <trafalgarlwa@noreply.gitea.io>
2022-06-12 01:58:48 +0800

    chore(web): 新增 web 模块 (#4)
    Reviewed-on: https://gitea.com/hosppy/hosppy/pulls/4

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" "$DOCKER_REGISTRY"
-
-services=(account-svc email-svc planck)
+services=(account-svc email-svc planck web)
 for service in "${services[@]}"
 do
   cd "$service" || exit 1
   image_name="$DOCKER_REPO/$service"
   tag_commit=$(git rev-parse --short HEAD)
-  docker build -t "$image_name" .
+  docker build -t "$image_name" . || exit 2
   docker tag "$image_name" "$image_name:$tag_commit"
-#  docker push "$image_name:latest"
-#  docker push "$image_name:$tag_commit"
   cd ..
 done
---
+++ web/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Login from './pages/Login.vue'</script>
+
+<template>
+  <Login></Login>
+</template>
