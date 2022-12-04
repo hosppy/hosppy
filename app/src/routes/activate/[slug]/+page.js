b9089b7881cc395b253a36105039c2fd62bdf5d8
tradlwa <1596633963@qq.com>
2022-12-04 23:57:17 +0800

    migrate to svelte project

---
+++ app/src/routes/activate/[slug]/+page.js
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit';
+
+/** @type {import('./$types').PageLoad} */
+export function load({ params }) {
+  if (params.slug === 'hello-world') {
+    return {
+      title: 'Hello world!',
+      content: 'Welcome to our blog. Lorem ipsum dolor sit amet...',
+      slug: params.slug
+    };
+  }
+
+  throw error(404, 'Not found');
+}
---
+++ app/src/routes/register/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+  let registered = false;
+</script>
+
+<div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div class="max-w-md w-full space-y-8">
+    <div>
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">注册账号</h2>
+    </div>
+    {#if registered}
+      <div class="mt-8 space-y-6">
+        <p class="text-gray-500 text-base leading-relaxed">
+          注册成功！激活链接已发送到邮箱
+          <span class="text-indigo-500" />，请注意查收！
+        </p>
+        <div>
+          <a
+            href="/login"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            已有账号？立即登录
+          </a>
+        </div>
+      </div>
+    {:else}
+      <form class="mt-8 space-y-6">
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">邮箱地址</label>
+            <input
+              type="email"
+              autocomplete="email"
+              required
+              class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              placeholder="邮箱"
+            />
+          </div>
+        </div>
+        <span class="flex items-center font-normal tracking-wide text-red-500 text-xs mt-1 ml-1">
+          <!-- {{ errMsg }} -->
+        </span>
+
+        <div>
+          <button
+            type="submit"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            注册
+          </button>
+        </div>
+        <div>
+          <a href="/login">
+            <button
+              class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              已有账号？立即登录
+            </button>
+          </a>
+        </div>
+      </form>
+    {/if}
+  </div>
+</div>
