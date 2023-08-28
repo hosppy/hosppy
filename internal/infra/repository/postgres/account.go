238c5c51bbdd697302a18975b16b8793b294b8ad
tradlwa <heminghu00@126.com>
2023-08-28 13:46:36 +0800

    fix account model convert

--- internal/infra/repository/postgres/account.go
+++ internal/infra/repository/postgres/account.go
@@ -1,7 +1,10 @@
 package postgres
 
 import (
+	"context"
+
 	"github.com/hosppy/oxcoding/ent"
+	"github.com/hosppy/oxcoding/ent/account"
 	"github.com/hosppy/oxcoding/internal/domain/entity"
 	"github.com/hosppy/oxcoding/internal/domain/repository"
 )
@@ -13,6 +16,21 @@ type AccountRepository struct {
 var _ repository.AccountRepository = (*AccountRepository)(nil)
 
 func (a *AccountRepository) FindByEmail(email string) *entity.Account {
-	//TODO implement me
-	panic("implement me")
+	if res := a.Account.Query().Where(account.Email(email)).FirstX(context.Background()); res != nil {
+		return toEntity(res)
+	}
+	return nil
+}
+
+func toEntity(a *ent.Account) *entity.Account {
+	return &entity.Account{
+		ID:           a.ID,
+		Email:        a.Email,
+		Name:         a.Name,
+		PhoneNumber:  a.PhoneNumber,
+		Active:       a.Active,
+		CreatedAt:    a.CreatedAt,
+		PasswordHash: a.PasswordHash,
+		AvatarURL:    a.AvatarURL,
+	}
 }
